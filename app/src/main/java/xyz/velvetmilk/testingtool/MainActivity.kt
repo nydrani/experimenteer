bbcb97f8336e11c98487e4b80a0fc026c98305c1
Nydrani <victorzhang6897@gmail.com>
2019-01-04 11:23:19 +1100

    Added basic fab and coordinator layout

--- app/src/main/java/xyz/velvetmilk/testingtool/MainActivity.kt
+++ app/src/main/java/xyz/velvetmilk/testingtool/MainActivity.kt
@@ -1,7 +1,9 @@
 package xyz.velvetmilk.testingtool
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +13,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         setSupportActionBar(toolbar)
+
+        fab.setOnClickListener {
+            Snackbar.make(it, R.string.test_message, Snackbar.LENGTH_LONG)
+                    .setAction(R.string.test_action) {
+                        Toast.makeText(this, R.string.test_message, Toast.LENGTH_SHORT).show()
+                    }
+                    .show()
+        }
     }
 }
